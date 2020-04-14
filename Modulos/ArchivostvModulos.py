77e2cfa8e590fef18068fe4c3667cf70e70ca4b4
djd2 <dj_d2_sesion@hotmail.com>
2020-04-14 12:34:07 +0200

    Add files via upload

--- Modulos/ArchivostvModulos.py
+++ Modulos/ArchivostvModulos.py
@@ -1,14 +1,57 @@
-# Embedded file name: /usr/lib/enigma2/python/Plugins/Extensions/archivostv/ArchivostvModulos.py
+# Embedded file name: /usr/lib/enigma2/python/Plugins/Extensions/archivostv/Modulos/ArchivostvModulos.py
 from xml.etree.cElementTree import fromstring, ElementTree
 import urllib2
 import urllib as ul
 import os, re
-from datetime import datetime
-from time import time
 
-def debug(obj, text=''):
-	print datetime.fromtimestamp(time()).strftime('[%H:%M:%S]')
-	print '%s' % text +  ' %s\n' % obj
+def Excepcion(texto = '', donde = ''):
+    print "Error: " + str(texto) + " " + str(donde)
+    print "Error: " + str(texto) + " " + str(donde)
+    print "Error: " + str(texto) + " " + str(donde)
+    
+def debug(texto = ''):
+    print str(texto)
+    
+def CambiaTexto(texto):
+    NN = texto
+    NN = NN.replace("¡","")
+    NN = NN.replace("¿","")
+    NN = NN.replace("?","")
+    NN = NN.replace(":","")
+    NN = NN.replace("º","")
+    NN = NN.replace("ª","")
+    NN = NN.replace("\"","")
+    NN = NN.replace("\'","")
+    NN = NN.replace("(","")
+    NN = NN.replace(")","")
+    NN = NN.replace("á","a")
+    NN = NN.replace("Á","A")
+    NN = NN.replace("é","e")
+    NN = NN.replace("É","E")
+    NN = NN.replace("í","i")
+    NN = NN.replace("Í","I")
+    NN = NN.replace("ó","o")
+    NN = NN.replace("Ó","O")
+    NN = NN.replace("ú","u")
+    NN = NN.replace("Ú","U")
+    NN = NN.replace("ñ","n")
+    NN = NN.replace("Ñ","N")
+    NN = NN.replace("&ntilde;","n")
+    NN = NN.replace("&quot;","")
+    NN = NN.replace("'","")
+    NN = NN.replace("&#039;","")
+    NN = NN.replace('\xc3\xb1','n')
+    NN = NN.replace('\xc3\x81','A')
+    NN = NN.replace('\xc3\xa1','a')
+    NN = NN.replace('\xc3\x8d','I')
+    NN = NN.replace('\xc3\xad','i')
+    NN = NN.replace('\xc3\x89','E')
+    NN = NN.replace('\xc3\xa9','e')
+    NN = NN.replace('\xc3\x93','O')
+    NN = NN.replace('\xc3\xb3','o')
+    NN = NN.replace('\xc3\x9a','U')
+    NN = NN.replace('\xc3\xba','u')
+    return NN
 
 def mod_request(url):
 	url = 'http://' + url
